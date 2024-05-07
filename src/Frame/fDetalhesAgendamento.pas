b2b0d865fec5199e218e9c646f7f976a8db6d5f1
Daniel Gomes <danielgommes1997@hotmail.com>
2024-05-07 13:01:44 -0300

    added new frame and unit

--- src/Frame/fDetalhesAgendamento.pas
+++ src/Frame/fDetalhesAgendamento.pas
@@ -4,10 +4,39 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
-  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls;
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  System.Skia, FMX.Objects, FMX.Layouts, FMX.Skia, FMX.Controls.Presentation;
 
 type
   TFrameDetalhesAgendamento = class(TFrame)
+    Layout1: TLayout;
+    RecImagem: TRectangle;
+    Layout3: TLayout;
+    LblDescricao: TLabel;
+    LblNome: TSkLabel;
+    Layout5: TLayout;
+    Layout6: TLayout;
+    Layout7: TLayout;
+    Rectangle1: TRectangle;
+    Layout15: TLayout;
+    Rectangle2: TRectangle;
+    Label9: TLabel;
+    Layout16: TLayout;
+    Rectangle3: TRectangle;
+    Label10: TLabel;
+    LblServico: TLabel;
+    Layout17: TLayout;
+    Rectangle4: TRectangle;
+    Label12: TLabel;
+    LblProfissional: TLabel;
+    Layout18: TLayout;
+    Rectangle5: TRectangle;
+    Label14: TLabel;
+    LblValor: TLabel;
+    Layout19: TLayout;
+    Rectangle6: TRectangle;
+    Label16: TLabel;
+    LblData: TLabel;
   private
     { Private declarations }
   public
--- src/uCheckout.pas
+++ src/uCheckout.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, System.Skia,
   FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, FMX.Skia, FMX.ListBox,
-  FMX.Objects;
+  FMX.Objects, fDetalhesAgendamento, uLoading;
 
 type
   TFrmCheckout = class(TForm)
@@ -20,7 +20,6 @@ type
     LoTopo: TLayout;
     Rectangle2: TRectangle;
     Layout3: TLayout;
-    Rectangle3: TRectangle;
     SkLabel1: TSkLabel;
     LoRodape: TLayout;
     Layout6: TLayout;
@@ -34,7 +33,21 @@ type
     Layout15: TLayout;
     Rectangle4: TRectangle;
     Label1: TLabel;
+    BtnVoltar: TRectangle;
+    ImgVoltar: TImage;
+    procedure AdicionarResumo(CodServico: Integer;
+                                       NomeLoja,
+                                       Descricao,
+                                       DescricaoServico,
+                                       NomeProfissional,
+                                       DataAgendamento,
+                                       Imagem: String;
+                                       Valor: Double);
+    procedure FormCreate(Sender: TObject);
+    procedure BtnVoltarClick(Sender: TObject);
   private
+    procedure ListarResumo;
+    procedure TerminateThread(Sender: TObject);
     { Private declarations }
   public
     { Public declarations }
@@ -46,5 +59,86 @@ var
 implementation
 
 {$R *.fmx}
+
+
+procedure TFrmCheckout.BtnVoltarClick(Sender: TObject);
+begin
+    Close;
+    FrmCheckout := nil;
+end;
+
+procedure TFrmCheckout.FormCreate(Sender: TObject);
+begin
+    ListarResumo;
+end;
+
+procedure TFrmCheckout.ListarResumo;
+var
+  Th: TThread;
+begin
+    TLoading.Show(FrmCheckout, 'Aguarde...');
+    LstResumo.Items.Clear;
+    Th := TThread.CreateAnonymousThread(procedure
+    begin
+        TThread.Synchronize(TThread.CurrentThread, procedure
+        begin
+            AdicionarResumo(1,
+                           'NomeBarber',
+                           'nossa barbearia é...........',
+                           'Corte do Jaca e barba',
+                           'Daniel',
+                           '08/05/2024 às 15:30',
+                           '',
+                           50.4);
+        end);
+    end);
+    th.OnTerminate := TerminateThread;
+    th.Start;
+end;
+
+procedure TFrmCheckout.TerminateThread(Sender : TObject);
+begin
+    if Assigned(TThread(Sender).FatalException) then
+       ShowMessage(Exception(TThread(Sender).FatalException).Message);
+    TLoading.Hide;
+end;
+
+procedure TFrmCheckout.AdicionarResumo(CodServico: Integer;
+                                       NomeLoja,
+                                       Descricao,
+                                       DescricaoServico,
+                                       NomeProfissional,
+                                       DataAgendamento,
+                                       Imagem: String;
+                                       Valor: Double);
+var
+  Item:  TListBoxItem;
+  Frame: TFrameDetalhesAgendamento;
+begin
+    //item vazio na listbox...
+    Item := TListBoxItem.Create(LstResumo);
+    Item.Text := '';
+    Item.Width := 460;
+    Item.Height := 275;
+    Item.Margins.Right := 10;
+    Item.Tag := CodServico;
+    Item.Selectable := false;
+
+    //criar o frame...
+    Frame := TFrameDetalhesAgendamento.Create(Item);
+    Frame.Parent := Item;
+    Frame.Align := TAlignLayout.Client;
+    Frame.LblNome.Text := NomeLoja;
+    Frame.LblDescricao.Text := Descricao;
+    Frame.LblServico.Text := DescricaoServico;
+    Frame.LblProfissional.Text := NomeProfissional;
+    Frame.LblData.Text := DataAgendamento;
+    Frame.LblValor.Text := FormatCurr('R$ #,##0.00', Valor);
+
+    //Frame.BtnAgendar.Tag := CodLoja; //armazenarCodigo
+    //Frame.BtnAgendar.OnClick   := ClickMenuOpcao;
+    //CarregarImagemDaURL(ImageBackgroundURL+ Imagem, Frame.Rectangle2);
+    LstResumo.AddObject(Item);
+end;
 
 end.
